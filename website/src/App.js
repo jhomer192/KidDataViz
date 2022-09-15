6e080b3e2bed7fb4ed6486ee522ca3bcd6a1deec
Jack Homer <jhomer19@vt.edu>
2022-09-15 16:39:04 -0400

    website started

---
+++ website/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import DropDownMenu from './dropdown.js'
+import React from "react";
+
+function App() {
+  
+  return (
+    
+    <div className="App">
+    
+
+      <header className="App-header">
+      <div>
+      <DropDownMenu />
+       <h1> Kid Data Viz</h1>
+       </div>
+        <p>
+          enter description of project
+        </p>
+        
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
