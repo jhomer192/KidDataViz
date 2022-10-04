72d2f4a0840fd268735dbeb41aef2b5ef4e6c6ef
Jack Homer <jhomer19@vt.edu>
2022-10-04 10:49:18 -0400

    skeleton fro home page done
    .

--- website/src/App.js
+++ website/src/App.js
@@ -1,7 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
-import DropDownMenu from './dropdown.js'
+
 import React from "react";
+import NavBar from "./navBar.js";
+import BarAcross from './barAcross';
+import GameWindow from './gameWindow';
 
 function App() {
   
@@ -9,18 +12,17 @@ function App() {
     
     <div className="App">
     
-
-      <header className="App-header">
-      <div>
-      <DropDownMenu />
-       <h1> Kid Data Viz</h1>
-       </div>
-        <p>
-          enter description of project
-        </p>
+    
+    
+      
+      <BarAcross/>
+      <NavBar />
+      <BarAcross/>
+      <GameWindow/>
+      <BarAcross/>
+       
         
-        
-      </header>
+    
     </div>
   );
 }
---
+++ website/src/gameWindow.js
@@ -0,0 +1,4 @@
+import React from "react"
+export default function GameWindow() {
+    return ( <h1>GameWindowHere</h1>
+    )};
