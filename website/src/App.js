ffcdc128275c26eaf8b2d7ea940a56e0b9d67751
Sasha Holt <sasha@holt.net>
2022-10-06 14:49:14 -0400

    Improved Toolbar + Dependencies
    Added formatting and components to the tool bar, added icons-material to dependencies.

--- website/src/App.js
+++ website/src/App.js
@@ -3,26 +3,14 @@ import './App.css';
 
 import React from "react";
 import NavBar from "./navBar.js";
-import BarAcross from './barAcross';
 import GameWindow from './gameWindow';
 
 function App() {
   
   return (
-    
     <div className="App">
-    
-    
-    
-      
-      <BarAcross/>
       <NavBar />
-      <BarAcross/>
       <GameWindow/>
-      <BarAcross/>
-       
-        
-    
     </div>
   );
 }
