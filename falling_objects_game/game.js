2945801edd65dda1583cce7272917aedb0f2b1df
aandrew17 <aandrew17@vt.edu>
2022-10-22 19:14:06 -0400

    updated falling objects game

--- falling_objects_game/game.js
+++ falling_objects_game/game.js
@@ -1,92 +1,125 @@
-var score = 0;
-var color = "blue";
+//Starting Point
 
-function random(min, max) {
-  return Math.round(Math.random() * (max - min) + min);
+var block = document.getElementById('block'),
+  brick = document.getElementById('brick'),
+  path = document.getElementById('path'),
+  gameover = document.getElementById('gameover')
+var blockRect = block.getBoundingClientRect(),
+  brickRect = brick.getBoundingClientRect()
+var left = 10;
+brick.style.left = 10;
+brick.style.top = 380;
+block.style.top = 10;
+block.style.left = Math.floor((Math.random() * 390) + 5);
+block.style.backgroundColor = "purple";
+gameover.style.top = 150;
+gameover.style.left = 150
+
+//Keys
+document.onkeydown = function () {
+  switch (window.event.keyCode) {
+    case 37:
+      moveleft(brick);
+      break;
+    case 39:
+      moveright(brick);
+      break;
+  }
+};
+
+//Movement
+function moveright(elem) {
+
+  function frame() {
+    if (elem.getBoundingClientRect().right <= 430) {
+      left = left + 20
+      elem.style.left = left + 'px'
+    }
+    clearInterval(id)
+  }
+  var id = setInterval(frame, 0)
 }
 
-function setBG() {
-  if (Math.round(Math.random())) {
-    return "http://icons.iconarchive.com/icons/hopstarter/halloween-avatars/128/Frankenstein-icon.png";
-  } else {
-    return "http://icons.iconarchive.com/icons/hopstarter/halloween-avatars/128/Scream-icon.png";
+function moveleft(elem) {
+
+  function frame() {
+    if (elem.getBoundingClientRect().left >= 30) {
+      left = left - 20
+      elem.style.left = left + 'px'
+    }
+    clearInterval(id)
   }
+  var id = setInterval(frame, 0)
 }
 
+var colors = ['pink', 'blue', 'purple', 'green', 'grey', 'yellow', 'orange']
 
-
-function dropBox() {
-  var length = random(100, ($(".game").width() - 100));
-  var velocity = random(850, 10000);
-  var size = random(50, 150);
-  var thisBox = $("<div/>", {
-    class: "box",
-    style: "width:" + size + "px; height:" + size + "px; left:" + length + "px; transition: transform " + velocity + "ms linear;"
-  });
-
-  //set data and bg based on data
-  thisBox.data("test", Math.round(Math.random()));
-  if (thisBox.data("test")) {
-    thisBox.css({ "background": "url('http://icons.iconarchive.com/icons/hopstarter/halloween-avatars/128/Frankenstein-icon.png')", "background-size": "contain" });
-  } else {
-    thisBox.css({ "background": "url('http://icons.iconarchive.com/icons/hopstarter/halloween-avatars/128/Scream-icon.png')", "background-size": "contain" });
-  }
-
-
-  //insert gift element
-  $(".game").append(thisBox);
-
-  //random start for animation
-  setTimeout(function () {
-    thisBox.addClass("move");
-  }, random(0, 5000));
-
-  //remove this object when animation is over
-  thisBox.one("webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend",
-    function (event) {
-      $(this).remove();
-    });
-}
-
-for (i = 0; i < 10; i++) {
-  dropBox();
-}
-
-$(document).on('click', '.box', function () {
-
-
-  if ($(this).data("test")) {
-    score += 1;
-  } else {
-    score -= 1;
-  }
-
-  $(".score").html(score);
-  $(this).remove();
-});
-
-var runGame = setInterval(function () {
-  for (i = 0; i < 10; i++) {
-    dropBox();
-  }
-}, 5000);
-
-function countdown() {
-  var seconds = 60;
-  function tick() {
-    var counter = document.getElementById("counter");
-    seconds--;
-    counter.innerHTML = (seconds < 10 ? "0" : "") + String(seconds) + "S";
-    if (seconds > 0) {
-      setTimeout(tick, 1000);
-      draw();
-      update();
-    } else {
-      alert("Game over");
-      clearInterval(runGame);
+function movedown() {
+  var top = 0
+  function frame() {
+    top++
+    block.style.top = top + 'px'
+    var blockRect = block.getBoundingClientRect(),
+      brickRect = brick.getBoundingClientRect()
+    if (block.style.top === '375px' && blockRect.right > brickRect.left && blockRect.left < brickRect.right) {
+      clearInterval(id)
+      updateScore()
+      block.style.top = 10;
+      block.style.left = Math.floor((Math.random() * 390) + 5);
+      block.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+      movedown()
+    }
+    if (top == 400) {
+      clearInterval(id);
+      document.getElementById("gameover").innerHTML = "GAME OVER";
     }
   }
-  tick();
+  var id = setInterval(frame, 2)
 }
 
-countdown();
+var score = 0
+function updateScore() {
+  score++
+  document.getElementById("score").innerHTML = score;
+}
+
+function reset() {
+  document.getElementById("gameover").innerHTML = "";
+  score = 0
+  document.getElementById("score").innerHTML = score;
+
+}
+
+//Swipe Functionality
+document.addEventListener('touchstart', handleTouchStart, false);
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;
+var yDown = null;
+
+function handleTouchStart(evt) {
+  xDown = evt.touches[0].clientX;
+  yDown = evt.touches[0].clientY;
+};
+
+function handleTouchMove(evt) {
+  if (!xDown || !yDown) {
+    return;
+  }
+
+  var xUp = evt.touches[0].clientX;
+  var yUp = evt.touches[0].clientY;
+  var xDiff = xDown - xUp;
+  var yDiff = yDown - yUp;
+
+  if (Math.abs(xDiff) > Math.abs(yDiff)) {
+    if (xDiff > 0) {
+      moveleft(brick)
+      console.log('hi')
+    } else {
+      moveright(brick)
+    }
+  }
+  xDown = null;
+  yDOwn = null;
+}
