24cf452157bde37a5417fe0a9f755e3f546a3629
Jack Homer <jhomer19@vt.edu>
2022-11-10 14:45:40 -0500

    minor change to game display, still WIP

--- website/src/componets/GameDisplay.js
+++ website/src/componets/GameDisplay.js
@@ -1,11 +1,17 @@
 import { imageListClasses, useThemeProps } from "@mui/material";
 import React from "react";
 import "./GameDisplay.css";
-
+import { Box } from "@mui/system";
+import Typography from "@mui/material";
 function GameDisplay(props)
 {
     return (
+        <Box>
+
         <img src={"./" + props.name+ ".png"}/>
+        <h1>{props.name}</h1>
+        </Box>
     );
+
 }
 export default GameDisplay;
