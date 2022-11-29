442c3f6cb952d740138589a07d054a90bb77d854
Heechan Lim <heechanlim@vt.edu>
2022-11-28 22:47:23 -0500

    Fixed Unity games and styling.

--- website/src/pages/CakeGame/CakeGamePage.jsx
+++ website/src/pages/CakeGame/CakeGamePage.jsx
@@ -1,23 +1,31 @@
 import React from 'react';
-import './CakeGamePage.css'
 import useScript from "../useScript"
 
 function CakeGame() {
-    useScript('Build/CakeGameDraft3.loader.js')
     useScript('CakeGame.js')
     return (
-
-      <>
-        <meta charSet="utf-8" />
-        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
-        <title>Unity WebGL Player | Cake Game</title>
-        <canvas
-          id="unity-canvas"
-          width={1080}
-          height={607}
-          style={{ cursor: "default", width: 1080, height: 607, background: "#231F20" }}
-        />
-      </>
+        <>
+          <meta charSet="utf-8" />
+          <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+          <title>Unity WebGL Player | Cake Game</title>
+          <link rel="shortcut icon" href="TemplateData/favicon.ico" />
+          <link rel="stylesheet" href="TemplateData/style.css" />
+          <div id="unity-container" className="unity-desktop">
+            <canvas id="unity-canvas" width={960} height={600} />
+            <div id="unity-loading-bar">
+              <div id="unity-logo" />
+              <div id="unity-progress-bar-empty">
+                <div id="unity-progress-bar-full" />
+              </div>
+            </div>
+            <div id="unity-warning"> </div>
+            <div id="unity-footer">
+              <div id="unity-webgl-logo" />
+              <div id="unity-fullscreen-button" />
+              <div id="unity-build-title"></div>
+            </div>
+          </div>
+        </>
     )
 }
 
--- website/src/pages/LegoGame/LegoGamePage.jsx
+++ website/src/pages/LegoGame/LegoGamePage.jsx
@@ -2,22 +2,30 @@ import React from 'react';
 import useScript from "../useScript"
 
 function LegoGame() {
-    useScript('Build/LegoGameDraft3.loader.js')
     useScript('LegoGame.js')
     return (
-
-    <>
-        <meta charSet="utf-8" />
-        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
-        <title>Unity WebGL Player | Cake Game</title>
-        <canvas
-          id="unity-canvas"
-          width={1080}
-          height={607}
-          style={{ width: 1080, height: 607, background: "#231F20" }}
-        />
-    </>
-      
+        <>
+          <meta charSet="utf-8" />
+          <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+          <title>Unity WebGL Player | Lego Game</title>
+          <link rel="shortcut icon" href="TemplateData/favicon.ico" />
+          <link rel="stylesheet" href="TemplateData/style.css" />
+          <div id="unity-container" className="unity-desktop">
+            <canvas id="unity-canvas" width={960} height={600} />
+            <div id="unity-loading-bar">
+              <div id="unity-logo" />
+              <div id="unity-progress-bar-empty">
+                <div id="unity-progress-bar-full" />
+              </div>
+            </div>
+            <div id="unity-warning"> </div>
+            <div id="unity-footer">
+              <div id="unity-webgl-logo" />
+              <div id="unity-fullscreen-button" />
+              <div id="unity-build-title"></div>
+            </div>
+          </div>
+        </>
     )
 }
 
